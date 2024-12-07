ec81a32dadc1cc6f6af14166eb6fa7df35287e80
Mehmet Emin Eker <Mehmet.Eker@d-teknoloji.com.tr>
2024-12-07 15:45:14 +0300

    feat: add ApiRepository and IApiRepository for API data

---
+++ Coreeple.Zigana/Coreeple.Zigana.Core/Data/Repositories/ApiRepository.cs
@@ -0,0 +1,25 @@
+﻿using Coreeple.Zigana.Core.Data.Entities;
+using Dapper;
+
+namespace Coreeple.Zigana.Core.Data.Repositories;
+
+public class ApiRepository(IDapperContext context) : IApiRepository
+{
+    public async Task<IEnumerable<Api>?> GetAll()
+    {
+        using var connection = context.CreateConnection();
+
+        var sql = """
+            SELECT "Id", "Path" FROM "Apis"
+        """;
+
+        var result = await connection.QueryAsync<Api>(sql);
+
+        return result;
+    }
+}
+
+public interface IApiRepository
+{
+    Task<IEnumerable<Api>?> GetAll();
+}
