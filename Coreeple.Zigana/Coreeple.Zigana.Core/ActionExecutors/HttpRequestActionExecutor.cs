dd9d84824174ca99ce1a43681ec1a4b0a937392b
Mehmet Emin Eker <Mehmet.Eker@d-teknoloji.com.tr>
2024-12-08 00:11:06 +0300

    feat: Add HttpRequestActionExecutor with ExecuteAsync method

---
+++ Coreeple.Zigana/Coreeple.Zigana.Core/ActionExecutors/HttpRequestActionExecutor.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Nodes;
+
+namespace Coreeple.Zigana.Core.ActionExecutors;
+public class HttpRequestActionExecutor
+{
+    public async Task<JsonNode?> ExecuteAsync(CancellationToken cancellationToken)
+    {
+        await Task.Delay(2000, cancellationToken);
+
+        return new JsonObject
+        {
+            ["status"] = "Ok",
+            ["content"] = new JsonObject
+            {
+                ["message"] = "Hello, World!"
+            }
+        };
+    }
+}
