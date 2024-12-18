a12201f14839052ad6cbce74a37eaff8e110df46
Mehmet Emin Eker <Mehmet.Eker@d-teknoloji.com.tr>
2024-12-18 13:41:54 +0300

    refactor: update StringValuesToObject method signatures

--- Coreeple.Zigana/Coreeple.Zigana.Core/Utils/HttpUtils.cs
+++ Coreeple.Zigana/Coreeple.Zigana.Core/Utils/HttpUtils.cs
@@ -8,22 +8,17 @@ using System.Xml;
 namespace Coreeple.Zigana.Core.Utils;
 public class HttpUtils
 {
-    public static Dictionary<string, object> StringValuesToObject(Dictionary<string, List<string>> values)
+    public static Dictionary<string, object> StringValuesToObject(Dictionary<string, IEnumerable<string>> values)
     {
         return values.Select(m => new KeyValuePair<string, object>(
                 m.Key,
-                m.Value.Count == 1 ? m.Value.First() : m.Value.ToArray()))
+                m.Value.Count() == 1 ? m.Value.First() : m.Value.ToArray()))
             .ToDictionary();
     }
 
     public static Dictionary<string, object> StringValuesToObject(Dictionary<string, StringValues> values)
     {
-        return StringValuesToObject(values.ToDictionary(m => m.Key, m => m.Value.Select(m => m!).ToList()));
-    }
-
-    public static Dictionary<string, object> StringValuesToObject(Dictionary<string, IEnumerable<string>> values)
-    {
-        return StringValuesToObject(values);
+        return StringValuesToObject(values.ToDictionary(m => m.Key, m => m.Value.Select(m => m!).AsEnumerable()));
     }
 
     public static string GetContentType(string contentType)
