3a98373f979b5d173859da489e719d6ba260c32c
Mehmet Emin Eker <Mehmet.Eker@d-teknoloji.com.tr>
2024-12-19 22:34:37 +0300

    feat: enhance AddZigana, add ZiganaHttpClient, middleware

--- Coreeple.Zigana/Coreeple.Zigana.AspNet/Extensions/ServiceCollectionExtensions.cs
+++ Coreeple.Zigana/Coreeple.Zigana.AspNet/Extensions/ServiceCollectionExtensions.cs
@@ -1,10 +1,35 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.Extensions.DependencyInjection;
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddZigana(this IServiceCollection services, IConfiguration configuration)
     {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(configuration);
+
+        services.AddHttpClient("ZiganaHttpClient")
+            .ConfigurePrimaryHttpMessageHandler(_ => new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) =>
+                {
+                    return true;
+                }
+            });
+
+        services.AddProblemDetails(options =>
+        {
+            options.CustomizeProblemDetails = context =>
+            {
+                var activity = context.HttpContext.Features.Get<IHttpActivityFeature>()?.Activity;
+
+                context.ProblemDetails.Instance = $"{context.HttpContext.Request.Method} {context.HttpContext.Request.Path}";
+                context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+                context.ProblemDetails.Extensions.TryAdd("traceId", activity?.Id);
+            };
+        });
+
         return services;
     }
 }
