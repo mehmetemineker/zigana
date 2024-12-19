f080c8d3763d0aa159759b88a98d9de9b466e921
Mehmet Emin Eker <Mehmet.Eker@d-teknoloji.com.tr>
2024-12-19 22:31:53 +0300

    feat: add new middlewares to UseZigana extension method

--- Coreeple.Zigana/Coreeple.Zigana.AspNet/Extensions/ApplicationBuilderExtensions.cs
+++ Coreeple.Zigana/Coreeple.Zigana.AspNet/Extensions/ApplicationBuilderExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Microsoft.AspNetCore.Builder;
+﻿using Coreeple.Zigana.AspNet.Middlewares;
+
+namespace Microsoft.AspNetCore.Builder;
 public static class ApplicationBuilderExtensions
 {
     public static IApplicationBuilder UseZigana(this IApplicationBuilder app)
@@ -7,6 +9,9 @@ public static class ApplicationBuilderExtensions
 
         app.UseExceptionHandler();
         app.UseStatusCodePages();
+
+        app.UseMiddleware<CustomTraceIdentifierMiddleware>();
+        app.UseMiddleware<HttpRequestHandlerMiddleware>();
 
         return app;
     }
