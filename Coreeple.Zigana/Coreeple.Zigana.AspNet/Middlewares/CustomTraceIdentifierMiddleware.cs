29fea82c31c40a70dbcf374301feedbadc1cb4cf
Mehmet Emin Eker <Mehmet.Eker@d-teknoloji.com.tr>
2024-12-19 22:18:56 +0300

    feat: add CustomTraceIdentifier and HttpRequestHandler middleware

---
+++ Coreeple.Zigana/Coreeple.Zigana.AspNet/Middlewares/CustomTraceIdentifierMiddleware.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Coreeple.Zigana.AspNet.Middlewares;
+
+public class CustomTraceIdentifierMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        await next(context);
+    }
+}
---
+++ Coreeple.Zigana/Coreeple.Zigana.AspNet/Middlewares/HttpRequestHandlerMiddleware.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Coreeple.Zigana.AspNet.Middlewares;
+public class HttpRequestHandlerMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        await next(context);
+    }
+}
