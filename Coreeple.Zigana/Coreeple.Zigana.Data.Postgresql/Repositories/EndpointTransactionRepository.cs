79684e9e1e206be087ce6efa3cbbe14878ed34f3
Mehmet Emin Eker <Mehmet.Eker@d-teknoloji.com.tr>
2024-12-21 15:51:05 +0300

    feat: Add async insert method to transaction repository

--- Coreeple.Zigana/Coreeple.Zigana.Data.Postgresql/Repositories/EndpointTransactionRepository.cs
+++ Coreeple.Zigana/Coreeple.Zigana.Data.Postgresql/Repositories/EndpointTransactionRepository.cs
@@ -1,6 +1,21 @@
 ﻿using Coreeple.Zigana.Data.Abstractions;
+using Coreeple.Zigana.Data.Entities;
+using Dapper;
 
 namespace Coreeple.Zigana.Data.Postgresql.Repositories;
-public class EndpointTransactionRepository : IEndpointTransactionRepository
+public class EndpointTransactionRepository(IDbContext dbContext) : IEndpointTransactionRepository
 {
+    public async Task<Guid> InsertAsync(EndpointTransaction endpointTransaction)
+    {
+        using var connection = dbContext.CreateConnection();
+
+        var sql = """
+            INSERT INTO "EndpointTransactions" ("Id", "EndpointId", "RequestId", "Name", "Status", "Date")
+            VALUES (@Id, @EndpointId, @RequestId, @Name, @Status, @Date)
+        """;
+
+        await connection.ExecuteAsync(sql, endpointTransaction);
+
+        return endpointTransaction.Id;
+    }
 }
